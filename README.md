bea88471429f9e0d4d9a5c3a4118037405425878
Remco Tolsma <info@remcotolsma.nl>
2015-11-11 13:45:02 +0100

    Added an integration class.

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 **Adyen driver for the WordPress payment processing library.**
 
 [![Built with Grunt](https://cdn.gruntjs.com/builtwith.png)](http://gruntjs.com/)
+
+## Production Environment
+
+**Dashboard URL:** https://ca-live.adyen.com/  
+**Payment Server URL:** https://live.adyen.com/hpp/pay.shtml  
+
+## Test Environment
+
+**Dashboard URL:** https://ca-test.adyen.com/  
+**Payment Server URL:** https://test.adyen.com/hpp/pay.shtml  
---
+++ src/Integration.php
@@ -0,0 +1,22 @@
+<?php
+
+class Pronamic_WP_Pay_Gateways_Adyen_Integration {
+	public function __construct() {
+		$this->id         = 'adyen';
+		$this->name       = 'Adyen';
+		$this->url        = 'http://www.adyen.com/';
+		$this->provider   = 'adyen';
+	}
+
+	public function get_config_factory_class() {
+		
+	}
+
+	public function get_config_class() {
+		
+	}
+
+	public function get_gateway_class() {
+		return 'Pronamic_WP_Pay_Gateways_Adyen_Gateway';
+	}
+}

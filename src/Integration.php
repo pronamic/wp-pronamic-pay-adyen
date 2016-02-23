f7220f51e0545712f3b933bdadf90e3076be9f36
Remco Tolsma <info@remcotolsma.nl>
2016-02-23 15:31:51 +0100

    Removed get_config_class(), no longer required.

--- src/Integration.php
+++ src/Integration.php
@@ -20,12 +20,4 @@ class Pronamic_WP_Pay_Gateways_Adyen_Integration {
 	public function get_config_factory_class() {
 
 	}
-
-	public function get_config_class() {
-
-	}
-
-	public function get_gateway_class() {
-		return 'Pronamic_WP_Pay_Gateways_Adyen_Gateway';
-	}
 }

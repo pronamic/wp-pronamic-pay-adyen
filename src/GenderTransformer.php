96a6e496d5c5366397b19c18df3bb23670c00937
Reüel van der Steege <reuel@pronamic.nl>
2019-02-28 12:35:23 +0100

    Oops, add missing gender transformer.

---
+++ src/GenderTransformer.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Gender transformer
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2019 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay\Gateways\Adyen
+ */
+
+namespace Pronamic\WordPress\Pay\Gateways\Adyen;
+
+use Pronamic\WordPress\Pay\Gender as Pay_Gender;
+
+/**
+ * Gender transformer
+ *
+ * @author  Reüel van der Steege
+ * @version 1.0.0
+ * @since   1.0.0
+ */
+class GenderTransformer {
+	/**
+	 * Transform WordPress Pay gender to Adyen amount.
+	 *
+	 * @param string $gender WordPress Pay gender to convert.
+	 *
+	 * @return string
+	 */
+	public static function transform( $gender ) {
+		switch ( $gender ) {
+			case Pay_Gender::FEMALE:
+				return Gender::FEMALE;
+
+			case Pay_Gender::MALE:
+				return Gender::MALE;
+
+			case Pay_Gender::OTHER:
+			default:
+				return Gender::UNKNOWN;
+		}
+	}
+}

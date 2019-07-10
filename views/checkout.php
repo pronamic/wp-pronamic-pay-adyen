2734fb377217b1872c7b4695e1d72a7f65373706
Reüel van der Steege <reuel@pronamic.nl>
2019-07-10 12:40:48 +0200

    Add viewport meta to checkout page for responsiveness.

--- views/checkout.php
+++ views/checkout.php
@@ -14,6 +14,7 @@
 <html <?php language_attributes(); ?>>
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
 		<title><?php esc_html_e( 'Checkout', 'pronamic_ideal' ); ?></title>
 

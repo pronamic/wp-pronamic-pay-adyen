c51b01672029ef9d99d4e31b19185c49d278bd9e
Reüel van der Steege <reuel@pronamic.nl>
2019-09-28 12:35:03 +0200

    Improved error message for HTTP 4xx and 500 responses (instead of JSON syntax error message).

--- src/Client.php
+++ src/Client.php
@@ -70,6 +70,18 @@ class Client {
 		// Body.
 		$body = wp_remote_retrieve_body( $response );
 
+		// Handle HTTP 4xx and 5xx response.
+		// @link https://docs.adyen.com/development-resources/response-handling
+		$response_code = wp_remote_retrieve_response_code( $response );
+
+		if ( in_array( $response_code, array( 400, 401, 403, 404, 422, 500 ), true ) ) {
+			throw new \Pronamic\WordPress\Pay\GatewayException(
+				'adyen',
+				sprintf( '%1$s (%2$s)', wp_remote_retrieve_response_message( $response ), $response_code ),
+				$response
+			);
+		}
+
 		$data = json_decode( $body );
 
 		// JSON error.

1241b89689e2502bf0b890af6cd54facd94d355c
Remco Tolsma <info@remcotolsma.nl>
2019-11-20 15:26:15 +0100

    Fixed PHPCS.

--- src/Client.php
+++ src/Client.php
@@ -73,7 +73,7 @@ class Client {
 
 		/**
 		 * On PHP 7 or higher the `json_decode` function will return `null` and
-		 * `json_last_error` will return `4` (Syntax error). On PHP 5.6 or lower 
+		 * `json_last_error` will return `4` (Syntax error). On PHP 5.6 or lower
 		 * the `json_decode` will also return `null`, but json_last_error` will
 		 * return `0` (No error). Therefor we check if the HTTP response body
 		 * is an empty string.

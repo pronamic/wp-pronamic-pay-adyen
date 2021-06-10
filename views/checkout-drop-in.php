048acfd11f9c2f736deb4ecf7e5ae740976007ed
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2021-06-10 13:25:21 +0200

    Move head tag content to it's own file so we can reuse it in the two views.

--- views/checkout-drop-in.php
+++ views/checkout-drop-in.php
@@ -13,23 +13,7 @@
 
 <html <?php language_attributes(); ?>>
 	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-
-		<title><?php esc_html_e( 'Checkout', 'pronamic_ideal' ); ?></title>
-
-		<?php
-
-		/**
-		 * Triggers on Adyen checkout page.
-		 *
-		 * @link https://github.com/wp-pay-gateways/adyen#pronamic_pay_adyen_checkout_head
-		 *
-		 * @since 1.1
-		 */
-		do_action( 'pronamic_pay_adyen_checkout_head' );
-
-		?>
+		<?php include __DIR__ . '/head.php'; ?>
 	</head>
 
 	<body>
---
+++ views/head.php
@@ -0,0 +1,17 @@
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+<title><?php esc_html_e( 'Checkout', 'pronamic_ideal' ); ?></title>
+
+<?php
+
+/**
+ * Triggers on Adyen checkout page.
+ *
+ * @link https://github.com/wp-pay-gateways/adyen#pronamic_pay_adyen_checkout_head
+ *
+ * @since 1.1
+ */
+do_action( 'pronamic_pay_adyen_checkout_head' );
+
+?>

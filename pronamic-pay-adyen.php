b2edf58fa94701093d6000fdc661e9b161bd1237
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2022-04-11 16:06:55 +0200

    3.1.0

--- pronamic-pay-adyen.php
+++ pronamic-pay-adyen.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://www.pronamic.eu/plugins/pronamic-pay-adyen/
  * Description: Extend the Pronamic Pay plugin with the Adyen gateway to receive payments with Adyen through a variety of WordPress plugins.
  *
- * Version: 3.0.0
+ * Version: 3.1.0
  * Requires at least: 4.7
  *
  * Author: Pronamic
